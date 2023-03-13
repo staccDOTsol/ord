bd35deccb192f62392abfbfec03ca8b82ef94ebb
Your Name <you@example.com>
2023-03-13 17:02:22 -0400

    nft candy machine

--- src/subcommand/wallet/candy.rs
+++ src/subcommand/wallet/candy.rs
@@ -9,7 +9,8 @@ use glob::glob;
 pub(crate) struct Transactions {
   #[clap(long, help = "Fetch at most <LIMIT> transactions.")]
   limit: Option<u16>,
-  toglob: String
+  toglob: String,
+  satoshis: i64
 }
 
 #[derive(Serialize, Deserialize)]
@@ -31,43 +32,47 @@ impl Transactions {
             None,
         )?
         {
-            if tx.detail.amount.ge(&SignedAmount::from_sat(1_000_000)) 
+            if tx.detail.amount.ge(&SignedAmount::from_sat(self.satoshis)) 
                 && tx.detail.category == GetTransactionResultDetailCategory::Receive
                 && tx.info.confirmations > 0 {
-                let files = glob(&self.toglob.to_string()).unwrap();
-                let addy = tx.detail.address.as_ref().unwrap().to_string();
-                for f in files {
-                    let file = f.unwrap();
-                    let fstr = file.to_str().unwrap().to_string();
-                    if fstr.contains("png") {
-                    
+                    let mut dont = false;
+                    let files = glob(&self.toglob.to_string()).unwrap();
+                    let addy = tx.detail.address.as_ref().unwrap().to_string();
+                    for f in files {
+                        let file = f.unwrap();
+                        let fstr = file.to_str().unwrap().to_string();
+                       
                         if fstr.contains(&addy) {
-                            
-                        }
-                        else {
-                            std::fs::rename(fstr, self.toglob.to_string() + &addy.to_owned())?;
-                            super::wallet::inscribe::Inscribe {
-                                fee_rate: FeeRate::try_from(1.0).unwrap(),
-                                commit_fee_rate: None,
-                                file: file,
-                                no_backup: true,
-                                satpoint: None,
-                                dry_run: false,
-                                no_limit: false,
-                                destination: Some(tx.detail.address.as_ref().unwrap().clone()),
-                            }
-                                .run(options.clone())?; 
-                            
-                                
-                                
-                            }
+                            dont = true;
                         }
                     }
-            }
-        output.push(Output {
-            transaction: tx.info.txid,
-            confirmations: tx.info.confirmations,
-        });
+                    if dont == false {
+                        let files = glob(&self.toglob.to_string()).unwrap();
+                        for f in files {
+                            let file = f.unwrap();
+                            let fstr = file.to_str().unwrap().to_string();
+                            if fstr.contains("png") {
+                            
+                                std::fs::rename(fstr, self.toglob.to_string() + &addy.to_owned())?;
+                                super::wallet::inscribe::Inscribe {
+                                    fee_rate: FeeRate::try_from(1.0).unwrap(),
+                                    commit_fee_rate: None,
+                                    file: file,
+                                    no_backup: true,
+                                    satpoint: None,
+                                    dry_run: false,
+                                    no_limit: false,
+                                    destination: Some(tx.detail.address.as_ref().unwrap().clone()),
+                                }
+                                    .run(options.clone())?; 
+                                }
+                            }
+                        }
+                }
+            output.push(Output {
+                transaction: tx.info.txid,
+                confirmations: tx.info.confirmations,
+            });
         }
 
         print_json(output)?;
