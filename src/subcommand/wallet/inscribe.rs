b331a13ec297e286558bbbd256af3b5482c0f825
Your Name <you@example.com>
2023-04-17 12:45:02 -0400

    alr revertttt

--- src/subcommand/wallet/inscribe.rs
+++ src/subcommand/wallet/inscribe.rs
@@ -248,12 +248,7 @@ impl Inscribe {
       },
       &reveal_script,
     );
-
-    reveal_tx.output[0].value = reveal_tx.output[0]
-      .value
-      .checked_sub(fee.to_sat())
-      .context("commit transaction output value insufficient to pay transaction fee")?;
-
+    
     if reveal_tx.output[0].value < reveal_tx.output[0].script_pubkey.dust_value().to_sat() {
       bail!("commit transaction output would be dust");
     }
